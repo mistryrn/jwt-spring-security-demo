93852c782790432079af8dd668d405ece350b4c3
Rakesh Mistry <mistryrn@mcmaster.ca>
2017-12-11 12:02:16 -0500

    Create WAR instead of JAR

--- src/main/java/org/zerhusen/JwtDemoApplication.java
+++ src/main/java/org/zerhusen/JwtDemoApplication.java
@@ -2,12 +2,18 @@ package org.zerhusen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class JwtDemoApplication {
+public class JwtDemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(JwtDemoApplication.class, args);
-        sys.out.pl ()
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    	return builder.sources(JwtDemoApplication.class);
     }
 }
